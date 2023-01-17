2c764ff71eabe59f641999e45552e21ac231efd4
shaikhshehzad <shehzadali3113@gmail.com>
2023-01-17 22:39:29 +0530

    learning how to redirect

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 7000;
-const path = require('path');
-const publicPath = path.join(__dirname, 'public');
+const path = require("path");
+const publicPath = path.join(__dirname, "public");
 
-app.get('/', (req, res) => {
-//   res.send('Wupphuuu server is live now !')
+app.get("/", (req, res) => {
+  //   res.send('Wupphuuu server is live now !')
 
-    res.sendFile(`${publicPath}/HomePage.html`);
+  res.sendFile(`${publicPath}/HomePage.html`);
+});
 
-})
+app.get("/about", (req, res) => {
+  res.send("From about");
+});
+app.get("/contactus", (req, res) => {
+  res.send("From contactus");
+});
 
-app.get('/about', (req, res) => {
-    res.send('From about')
-  })
-app.get('/contactus', (req, res) => {
-    res.send('From contactus')
-})
+app.get("/redirection", (req, res) => {
+  console.log("redirecting");
+  res.redirect("/redirected");
+});
 
-app.get('*', (req, res) => {
-      res.sendFile(`${publicPath}/Error.html`);
-})
+app.get("/redirected", (req, res) => {
+  res.send("From redirected page");
+});
 
+app.get("*", (req, res) => {
+  res.sendFile(`${publicPath}/Error.html`);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server app listening on port ${PORT}`)
-})
+  console.log(`Server app listening on port ${PORT}`);
+});
