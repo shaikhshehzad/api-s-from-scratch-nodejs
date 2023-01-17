cbf18285eafb4ae067e989ccf727ff2948279a2b
shaikhshehzad <shehzadali3113@gmail.com>
2023-01-17 19:45:46 +0530

    setting up BE routes and html files

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 7000;
+const path = require('path');
+const publicPath = path.join(__dirname, 'public');
 
 app.get('/', (req, res) => {
-  res.send('Wupphuuu server is live now !')
+//   res.send('Wupphuuu server is live now !')
+
+    res.sendFile(`${publicPath}/HomePage.html`);
+
 })
 
 app.get('/about', (req, res) => {
