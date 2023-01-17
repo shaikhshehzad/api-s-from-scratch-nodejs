349447eb99779e7adaa58f6b910d2e433ca6ebe5
shaikhshehzad <shehzadali3113@gmail.com>
2023-01-17 19:31:37 +0530

    enviornment variable fix

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 7000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Wupphuuu server is live now !')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/about', (req, res) => {
+    res.send('From about')
+  })
+app.get('/contactus', (req, res) => {
+    res.send('From contactus')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server app listening on port ${PORT}`)
 })
